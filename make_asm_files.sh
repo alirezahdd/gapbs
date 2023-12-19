584521e46fe0608064b91657301aa1a392f56202
Alireza-work <a71.haddadi@gmail.com>
2023-12-19 20:43:11 +0100

    updated

--- make_asm_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir asmsrc
-kernels=("bc" "bfs" "cc" "pr" "sssp" "tc")
-
-for i in "${!kernels[@]}"
-do
-    objdump -S -l --visualize-jump ${kernels[$i]} > asmsrc/${kernels[$i]}.asm
-done
